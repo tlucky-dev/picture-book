ce3dd129ac01d8cac1f8d2aac1ff2aeacb06d179
marvin <j123211@qq.com>
2023-11-15 13:55:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,20 @@ import sizeOf from 'npm:image-size';
 createPPT('1212', ['pics/1.jpg', 'pics/2.jpg'])
 
 function createPPT(fileName, pics) {
-  let pptx = new pptxgen()
+  let pptx = new pptxgen();
+  const width = 11.69;
+  const height = 8.27;
   // 初始化纸张大小
-  pptx.defineLayout({ name:'A4', width:11.69, height:8.27 });
+  pptx.defineLayout({
+    name: 'A4',
+    width: width,
+    height: height
+  });
   pptx.layout = 'A4';
 
   for (let index = 0; index < pics.length; index++) {
     const pic = pics[index];
-    pptx.addSlide().addImage(genImagesParams(pic));
+    pptx.addSlide().addImage(genImagesParams(pic, width, height));
   }
 
   pptx.writeFile({
@@ -19,7 +25,7 @@ function createPPT(fileName, pics) {
   });
 }
 
-function genImagesParams(picPath) {
+function genImagesParams(picPath, pageWidth, pageHeight) {
   const metadata = sizeOf(picPath)
   var params = {
     path: picPath,
@@ -30,14 +36,14 @@ function genImagesParams(picPath) {
   }
   if (metadata.width < metadata.height) {
     // 只保证高度撑满页面即可，宽度随意
-    params.h = 8.27
-    params.w = parseFloat((8.27 * (metadata.width / metadata.height)))
-    params.x = parseFloat(((11.69  - params.w) / 2))
+    params.h = pageHeight
+    params.w = parseFloat((pageHeight * (metadata.width / metadata.height)))
+    params.x = parseFloat(((pageWidth  - params.w) / 2))
   } else {
     // 保证宽度，高度居中(可裁剪)
-    params.w = 11.69
-    params.h = parseFloat((11.69 * (metadata.height / metadata.width)))
-    params.y = parseFloat(((8.27  - params.h) / 2))
+    params.w = pageWidth
+    params.h = parseFloat((pageWidth * (metadata.height / metadata.width)))
+    params.y = parseFloat(((pageHeight  - params.h) / 2))
   }
   return params;
 }
